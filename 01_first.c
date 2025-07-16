146f12f88596e8865e0470f537283c7a4dde1ff2
chetan8085patel@gmail.com <chetan8085patel@gmail.com>
2025-07-16 21:17:36 +0530

    16/07

---
+++ 01_first.c
@@ -0,0 +1,15 @@
+//here stdio.h is a library/header file   
+#include<stdio.h>
+void main()
+{
+   printf("hello\t\t\tstudents\n");  //  \n is used to line break
+   printf("how are you ?\n");
+   printf("how are you ?\n");
+  // printf("how are you ?\n");
+  // printf("how are you ?\n");	
+}
+
+/*this is
+multiline 
+comment */
+
