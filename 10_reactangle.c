9c8b75987f6f85e4f0785c777169c4b3eaa8e003
chetan8085patel@gmail.com <chetan8085patel@gmail.com>
2025-07-19 20:13:50 +0530

    19/07

---
+++ 10_reactangle.c
@@ -0,0 +1,12 @@
+// wap to find area of react angle.
+#include <stdio.h>
+void main()
+{
+  float length, width, area;
+  printf("enter length : ");
+  scanf("%f", &length);
+  printf("enter width : ");
+  scanf("%f", &width);
+  area = length * width;
+  printf("area of reactangle = %.2f", area);
+}
