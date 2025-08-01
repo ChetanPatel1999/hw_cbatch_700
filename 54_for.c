a76bb17f510b541267047811351f068e28182bf6
chetan8085patel@gmail.com <chetan8085patel@gmail.com>
2025-08-01 19:26:31 +0530

    31/7

---
+++ 54_for.c
@@ -0,0 +1,10 @@
+// wap to print 10 to 1 number.
+#include <stdio.h>
+void main()
+{
+    int i;
+    for (i = 10; i >= 1; i--) // 0
+    {
+        printf("%d\n", i); // 1
+    }
+}
---
+++ 55_for.c
@@ -0,0 +1,10 @@
+// wap to print square of 1 to 10.
+#include <stdio.h>
+void main()
+{
+    int i;
+    for (i = 1; i <= 10; i++) // 0
+    {
+        printf("square of %d = %d\n", i, i * i); // 1
+    }
+}
