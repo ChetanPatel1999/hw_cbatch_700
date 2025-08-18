f69be4fa69324d888a2a240d3ece015d4da39c78
chetan8085patel@gmail.com <chetan8085patel@gmail.com>
2025-08-18 19:17:53 +0530

    08/14

---
+++ 104_nested_loop.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+void main()
+{
+    int i, j;
+    for (i = 1; i <= 5; i++) // i= 6
+    {
+        for (j = 1; j <= 4; j++) // j=5
+        {
+            printf("hello students \n"); // 20
+        }
+        printf("-----------------------------\n");//5
+    }
+    
+}
